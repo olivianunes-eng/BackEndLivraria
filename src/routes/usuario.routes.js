3c9491a483a51019e6e7d05b3fa4c8c270149b48
OLIVIA SANTOS NUNES <25162445@edu119.local>
2025-10-31 11:02:04 -0300

    Primeiro commit

---
+++ src/routes/usuario.routes.js
@@ -0,0 +1,18 @@
+import{
+    criarUsuario,
+    listaUsuario,
+    obterUsuario,
+    atualizarUsuario,
+    deletarUsuario} from '../controllers/usuario.controller.js';
+
+import express from 'express';
+
+const router = express.Router();
+
+router.get("/", listaUsuario);
+router.post("/", criarUsuario);
+router.get("/:id", obterUsuario);
+router.put("/:id", atualizarUsuario);
+router.delete("/:id", deletarUsuario);
+
+export default router;
